3e27871fadac17f5b0e091ad434c8a74bbc73ae7
Jose Moreno <jomoreno@teksystems.com>
2020-11-17 17:14:04 -0600

    passing junit test for plain burger with default values

--- src/main/java/model/Burger.java
+++ src/main/java/model/Burger.java
@@ -2,23 +2,29 @@ package model;
 
 public abstract class Burger {
 
-    protected final BreadType bread;
-    protected final MeatType meat;
-    
-    protected abstract MeatType getMeat();
 
-    protected enum BreadType {WHITE}
+    protected enum BreadType { WHITE, WHEAT, RYE, SOURDOUGH }
+    protected final BreadType bread;
+    public enum MeatType { BEEF, CHICKEN, BLACK_BEAN }
+    protected final MeatType meat;
+
+    protected abstract BreadType getBread();
+    protected abstract MeatType getMeat();
 
     protected Burger(Builder builder) {
         this.bread = builder.bread;
         meat = builder.meat;
     }
 
-    protected abstract BreadType getBread();
-
-    public enum MeatType {
-        BEEF
+    private Burger() {
+        this.bread = BreadType.WHITE;
+        this.meat = MeatType.BEEF;
     }
+
+    public final static Builder getPlainInstance() {
+       return new PlainBurger.Builder();
+    }
+
 
     protected abstract static class Builder {
         protected BreadType bread;
--- src/test/java/model/BurgerTest.java
+++ src/test/java/model/BurgerTest.java
@@ -7,7 +7,9 @@ public class BurgerTest {
 
     @Test
     public void should_create_plain_burger_with_default_breadType_white_meatType_beef() {
-        Burger plainBurger = new PlainBurger.Builder().build();
+        //Burger plainBurger = new PlainBurger.Builder().build();
+
+        Burger plainBurger = Burger.getPlainInstance().build();
 
         Burger.BreadType expectedBread = Burger.BreadType.WHITE;
         Burger.BreadType actualBread = plainBurger.getBread();
@@ -19,4 +21,10 @@ public class BurgerTest {
 
         Assert.assertEquals(actualMeat, expectedMeat);
     }
+
+//    @Test
+//    public void should_create_plain_burger_with_given_bread_and_meat_type() {
+//        Burger aBurger = new BurgerBuilder().withBread(SOURDOUGH).withMeat(CHICKEN).withTopping(MAYO).build();
+//
+//    }
 }
